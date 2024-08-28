6d5a4fc2786bbfe71e5632ad4271699ce1a09c5a
Ho Thanh Thuy Tien <hthanhthuytien@gmail.com>
2024-08-28 14:56:48 +0700

    practical 4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -9,6 +9,9 @@ import android.view.CollapsibleActionView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,8 +63,23 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.CYAN);
+
+        // Inflate the layout for this fragment
+        TextView day = new TextView(getContext());
+        day.setText("Thursday");
+
+        ImageView img = new ImageView(getContext());
+        img.setImageResource(R.drawable.ic_weather);
+
+        View v = new View(getContext());
+        v.setBackgroundColor(Color.CYAN);
+
+        LinearLayout view = new LinearLayout(getContext());
+        view.setOrientation(LinearLayout.VERTICAL);
+        view.addView(day);
+        view.addView(img);
+        view.addView(v);
+//        return inflater.inflate(R.layout.fragment_forecast, container, false);
         return view;
     }
 }
